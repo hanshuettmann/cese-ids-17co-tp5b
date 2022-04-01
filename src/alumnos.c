49ea2fb4ef6ad3bf8de9f6c48aff99f23cfa71a0
HansHuettmann <hans.huttmann1@gmail.com>
2022-03-31 22:16:39 -0300

    Se agregan los datos del alumno HÜTTMANN, Patricio Hans (fix)

--- src/alumnos.c
+++ src/alumnos.c
@@ -25,6 +25,12 @@
 
 /*=====[Definitions of public global variables]==============================*/
 
+static const struct alumno_s ESTEBAN_VOLENTINI = {
+    .apellidos = "VOLENTINI",
+    .nombres = "Esteban Daniel",
+    .documento = "23.517.968",
+};
+
 static const struct alumno_s HANS_HUETTMANN = {
     .apellidos = "HÜTTMANN",
     .nombres = "Patricio Hans",
@@ -32,6 +38,7 @@ static const struct alumno_s HANS_HUETTMANN = {
 };
 
 const alumno_t ALUMNOS[] = {
+    &ESTEBAN_VOLENTINI,
     &HANS_HUETTMANN,
 };
 
